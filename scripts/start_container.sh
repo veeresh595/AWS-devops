c7356aef051c47eb50b8685658a5b08f7c865083
veeresh tegur <78965635+veeresh595@users.noreply.github.com>
2024-01-09 19:51:53 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull veeresh595/awscicd
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 veeresh595/awscicd
